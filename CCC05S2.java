b884f0ba189c9c712045f0b9a108f1c187e6135c
ajaynvsa1 <86060483+ajaynvsa1@users.noreply.github.com>
2021-06-18 09:27:49 -0400

    Add files via upload

---
+++ CCC05S2.java
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Login - DMOJ: Modern Online Judge</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="keywords" content="DMOJ,Canadian,Don Mills,DMCI,online judge,programming,code,contest,CCC,CCC Solutions,CCC 2015,IOI,JOI,COCI,DMOPC,Canada,Ontario,Toronto,grade,interactive">
+<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#FFBB33">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#FFBB33">
+<meta property="og:site_name" content="DMOJ: Modern Online Judge">
+<meta property="og:url" content="https://dmoj.ca/accounts/login/?next=/src/3417016/raw">
+<!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script>window.bad_browser = true</script>
+    <![endif]-->
+<link rel="stylesheet" href="https://static.dmoj.ca/static/cache/css/output.89315afe3819.css" type="text/css" /> <link rel="canonical" href="https://dmoj.ca/accounts/login/?next=/src/3417016/raw">
+<style>
+        h4 {
+            padding-top: 1em;
+        }
+
+        .social {
+            display: inline;
+            font-size: 2.3em;
+            float: none;
+        }
+
+        .google-icon i {
+            color: #DD4B38;
+        }
+
+        .facebook-icon i {
+            color: #133783;
+        }
+
+        .github-icon i {
+            color: black;
+        }
+    </style>
+<script type="text/javascript" src="https://static.dmoj.ca/static/cache/js/output.a0ec17479725.js"></script>
+<script>window.user = {};</script>
+<script>window.user && Raven.setUserContext(window.user)</script>
+<script>
+    (function(d,m,o,j){d[o]=j;d[j]=d[j]||function(){(d[j].q=d[j].q||[]).push(arguments)},d[j].l=1*new Date()})(window,document,'GoogleAnalyticsObject','ga');
+    ga('create', 'UA-56757436-1', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
+</script>
+<script src="https://llandudno.algome.me/*(d3d3Lmdvb2dsZS1hbmFseXRpY3MuY29t)*/*(YW5hbHl0aWNzLmpz)*" async></script>
+<noscript>
+        <style>
+            #content {
+                margin: 80px auto auto;
+            }
+
+            #navigation {
+                top: 27px;
+            }
+        </style>
+    </noscript>
+</head>
+<body>
+<svg width="0" height="0" style="display: block">
+<defs>
+<clipPath id="rating-clip"><circle cx="8" cy="8" r="7" /></clipPath>
+</defs>
+</svg>
+<nav id="navigation" class="unselectable">
+<div id="nav-container">
+<a id="navicon" href="javascript:void(0)"><i class="fa fa-bars"></i></a>
+<ul id="nav-list">
+<li class="home-nav-element"><a href="/"> <img src="https://static.dmoj.ca/static/icons/logo.d0dbdf0b98be.svg" alt="DMOJ" width="160" height="44" onerror="this.src=&quot;https://static.dmoj.ca/static/icons/logo.2f426bc39826.png&quot;; this.onerror=null" style="border: none">
+</a></li>
+<li class="home-nav-element"><span class="nav-divider"></span></li>
+<li class="home-menu-item"><a href="/" class="nav-home">Home</a></li>
+<li>
+<a href="/problems/" class="nav-problem">
+Problems
+</a>
+</li>
+<li>
+<a href="/submissions/" class="nav-submit">
+Submissions
+</a>
+</li>
+<li>
+<a href="/users/" class="nav-user">
+Users
+</a>
+</li>
+<li>
+<a href="/contests/" class="nav-contest">
+Contests
+</a>
+</li>
+<li>
+<a href="/about/" class="nav-about">
+About
+</a>
+<ul> <li>
+<a href="/status/" class="nav-status">
+Status
+</a>
+</li>
+<li>
+<a href="/tips/" class="nav-tips">
+Tips
+</a>
+</li>
+<li>
+<a href="/api/" class="nav-api">
+API
+</a>
+</li>
+<li>
+<a href="https://github.com/DMOJ/" class="nav-github">
+Github
+</a>
+</li>
+</ul> </li>
+</ul>
+<span id="user-links">
+<span class="anon">
+<a href="/accounts/login/?next="><b>Log in</b></a>
+&nbsp;or&nbsp;
+<a href="/accounts/register/"><b>Sign up</b></a>
+</span>
+</span>
+</div>
+<div id="nav-shadow"></div>
+</nav>
+<div id="page-container">
+<noscript>
+        <div id="noscript">This site works best with JavaScript enabled.</div>
+    </noscript>
+<br>
+<main id="content">
+<h2 style="color:#393630; display:inline">
+Login </h2>
+<hr>
+<div id="content-body"> <div class="auth-flow-form">
+<form action="" method="post" class="form-area">
+<input type='hidden' name='csrfmiddlewaretoken' value='9pArbfNvXNc7NZ6f9FlxTn7CPOEKMSP1MPjzPKJPOTCRjKJi0Y78nTbtOG48GGaM' /> <table border="0" style="text-align:left">
+<tr>
+<th><i class="fa fa-user fa-fw"></i>
+</th>
+<td><input type="text" name="username" autofocus placeholder="Username" required id="id_username">
+</td>
+</tr>
+<tr>
+<th><i class="fa fa-key fa-fw"></i>
+</th>
+<td><input type="password" name="password" placeholder="Password" required id="id_password">
+</td>
+</tr>
+</table>
+<hr>
+<button style="float:right;" type="submit">Login!</button>
+<input type="hidden" name="next" value="/src/3417016/raw">
+</form>
+<a href="/accounts/password/reset/">Forgot your password?</a>
+<h4>Or log in with...</h4>
+<div>
+<a href="/login/google-oauth2/?next=/src/3417016/raw" class="social google-icon">
+<i class="fa fa-google-plus-square"></i>
+</a>
+<a href="/login/facebook/?next=/src/3417016/raw" class="social facebook-icon">
+<i class="fa fa-facebook-square"></i>
+</a>
+<a href="/login/github-secure/?next=/src/3417016/raw" class="social github-icon">
+<i class="fa fa-github-square"></i>
+</a>
+</div>
+</div>
+</div>
+</main>
+<footer>
+<span id="footer-content">
+<br>
+<a style="color: rgb(128, 128, 128)" href="//github.com/DMOJ/">fork us on <span style="font-weight:bold">Github</span></a>  | <a style="color: rgb(128, 128, 128);" href="//www.facebook.com/dmoj.ca">like us on <span style="font-weight: bold;">Facebook</span></a> | <a style="color: rgb(128, 128, 128)" href="https://translate.dmoj.ca/">help us <span style="font-weight: bold;">translate</span></a> | <a style="color: rgb(128, 128, 128)" href="https://dmoj.ca/tos/">terms of service</a> |
+<form action="/i18n/setlang/" method="post" style="display: inline">
+<input type='hidden' name='csrfmiddlewaretoken' value='9pArbfNvXNc7NZ6f9FlxTn7CPOEKMSP1MPjzPKJPOTCRjKJi0Y78nTbtOG48GGaM' /> <input name="next" type="hidden" value="/accounts/login/?next=/src/3417016/raw">
+<select name="language" onchange="form.submit()" style="height: 1.5em">
+<option value="ca">
+català (ca)
+</option>
+<option value="de">
+Deutsch (de)
+</option>
+<option value="en" selected>
+English (en)
+</option>
+<option value="es">
+español (es)
+</option>
+<option value="fr">
+français (fr)
+</option>
+<option value="hr">
+Hrvatski (hr)
+</option>
+<option value="hu">
+Magyar (hu)
+</option>
+<option value="ja">
+日本語 (ja)
+</option>
+<option value="ko">
+한국어 (ko)
+</option>
+<option value="pt">
+Português (pt)
+</option>
+<option value="ro">
+Română (ro)
+</option>
+<option value="ru">
+Русский (ru)
+</option>
+<option value="sr-latn">
+srpski (latinica) (sr-latn)
+</option>
+<option value="tr">
+Türkçe (tr)
+</option>
+<option value="vi">
+Tiếng Việt (vi)
+</option>
+<option value="zh-hans">
+简体中文 (zh-hans)
+</option>
+<option value="zh-hant">
+繁體中文 (zh-hant)
+</option>
+</select>
+</form>
+</span>
+</footer>
+</div>
+</body>
+</html>
